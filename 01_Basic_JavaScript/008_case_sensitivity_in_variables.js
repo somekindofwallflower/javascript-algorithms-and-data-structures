db3e1a2f8e8606461440d7c12b1ad85a24d1d8b5
somekindofwallflower <someone.ana13@gmail.com>
2022-01-22 22:18:02 +0100

    Write varibles name in camelCase

---
+++ 01_Basic_JavaScript/008_case_sensitivity_in_variables.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// All variables and function names are case sensitive
+
+// Task: Modify the existing declarations and assignments so their names use camelCase.
+/*
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;*/
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
