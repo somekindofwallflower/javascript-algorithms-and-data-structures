34203b08056f0e099e16bc4f6c38a9f89cf45ecd
somekindofwallflower <someone.ana13@gmail.com>
2022-01-22 21:43:56 +0100

    Initializing variables with assignment operator

---
+++ 01_Basic_JavaScript/05_initializing_variables_with_assignment_operator.js
@@ -0,0 +1,9 @@
+// Initializing Variables with the Assignment Operator
+
+/*
+It is common to initialize a variable to an initial value in the same line as it is declared.
+    var myVar = 0;
+Creates a new variable called myVar and assigns it an initial value of 0.*/
+
+// Task: Define a variable a with var and initialize it to a value of 9./
+var a = 9;
