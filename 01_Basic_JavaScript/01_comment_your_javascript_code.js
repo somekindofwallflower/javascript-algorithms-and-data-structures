21f840c41fc3788152d76875b47a59840de3e8fb
somekindofwallflower <someone.ana13@gmail.com>
2022-01-22 15:33:43 +0100

    Create a file which contains two ways of commenting cde in javaScript

---
+++ 01_Basic_JavaScript/01_comment_your_javascript_code.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+Comments are lines of code that JavaScript will intentionally ignore.
+Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.*/
+
+// There are two ways to write comments in JavaScript:
+
+// Using // will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:
+// This is an in-line comment.
+
+
+// You can make a multi-line comment beginning with /* and ending with */. This is a multi-line comment:
+/* This is a
+multi-line comment */
+
+// Tasks
+
+// 1- You should create a // style comment that contains at least five letters.
+
+// Fist inline comment
+
+// 2- You should create a /* */ style comment that contains at least five letters.
+
+/* This is a
+multi-line comment */
