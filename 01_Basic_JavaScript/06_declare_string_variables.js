2b7e501f54644ffc548f6b9db4be8d8c983dcc43
somekindofwallflower <someone.ana13@gmail.com>
2022-01-22 21:50:38 +0100

    Declare string variables

---
+++ 01_Basic_JavaScript/06_declare_string_variables.js
@@ -0,0 +1,9 @@
+/*
+Declare String Variables
+var myName = "your name";
+"your name" is called a string literal. A string literal, or string, is a series of zero or more characters enclosed in single or double quotes.*/
+
+// Task: Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+var myFirstName = "Some Kind of";
+var myLastName = "Wallflower";
