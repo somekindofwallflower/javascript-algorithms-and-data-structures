66784e1f8a6c574065af12a14eff817e3b5cce37
somekindofwallflower <someone.ana13@gmail.com>
2022-01-22 22:25:37 +0100

    Variables declared with var and let difference

---
+++ 01_Basic_JavaScript/009_let_var_keywords.js
@@ -0,0 +1,11 @@
+// Differences Between the var and let Keywords
+
+// Declared variables with the var keyword can easily be overwritten
+// So unlike var, when you use let, a variable with the same name can only be declared once.
+
+// Update the code, so it only uses the let keyword.
+// var catName = "Oliver";
+// var catSound = "Meow!";
+
+let catName = "Oliver";
+let catSound = "Meow!";
