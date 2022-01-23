9d24f35bc21cecd41b410fc48968e9ee935654e9
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 14:32:55 +0100

    Multiply two decimals using * operator

---
+++ 01_Basic_JavaScript/018_multiply_two_decimals_with_javaScript.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// In JavaScript, you can also perform calculations with decimal numbers, just like whole numbers.
+
+/*
+Task: Change the 0.0 so that product will equal 5.0.
+
+    const product = 2.0 * 0.0;
+
+*/
+
+const product = 2.0 * 2.5;
+console.log("Product", product);
