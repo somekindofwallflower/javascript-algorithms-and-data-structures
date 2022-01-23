96aabff15389969893ffd85e00156b10e963de84
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 14:37:04 +0100

    Divide one decimal by another

---
+++ 01_Basic_JavaScript/019_divide_one_decimal_by_another_with_javaScript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/*
+Task: Change the 0.0 so that quotient will equal to 2.2.
+    const quotient = 0.0 / 2.0; // Change this line
+*/
+
+const quotient = 4.4 / 2.0;
+console.log("Quotient", quotient)
