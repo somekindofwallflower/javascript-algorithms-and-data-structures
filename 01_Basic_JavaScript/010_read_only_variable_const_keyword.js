abfdd497a8412c5c0e8b5bace48fd816cc9213ef
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 13:40:36 +0100

    Declare readonly variables using const instead of var and let

---
+++ 01_Basic_JavaScript/010_read_only_variable_const_keyword.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// In ES6, you can also declare variables using the const keyword.
+// Declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned:
+/*
+Task: Change the code so that all variables are declared using let or const.
+      Use let when you want the variable to change, and const when you want the variable to remain constant.
+      Also, rename variables declared with const to conform to common practices, meaning constants should be in all caps.*/
+
+/*
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line*/
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact);
