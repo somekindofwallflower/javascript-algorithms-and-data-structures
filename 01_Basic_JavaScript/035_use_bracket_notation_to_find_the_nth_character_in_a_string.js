c6e857466223596d3d3521a87e8b5a08c4a26565
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 19:00:28 +0100

    Get the third character of the string using the bracket notation

---
+++ 01_Basic_JavaScript/035_use_bracket_notation_to_find_the_nth_character_in_a_string.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// You can also use bracket notation to get the character at other positions within a string
+
+/*
+Task: Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName; // Change this line
+
+*/
+
+const lastName = "Lovelace";
+const thirdLetterOfLastName = lastName[2];
+console.log("Third letter of last name: ", thirdLetterOfLastName);
