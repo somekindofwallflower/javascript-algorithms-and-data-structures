888ff9a6b7405f67510da8ef69b1ae607a1f05cf
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 15:16:03 +0100

    Compound -/ to do the division and assignment in one step

---
+++ 01_Basic_JavaScript/024_compound_assignment_with_augmented_division.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// The /= operator divides a variable by another number.
+
+/*
+Task: Convert the assignments for a, b, and c to use the /= operator.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a = a / 12;
+b = b / 4;
+c = c / 11;
+
+*/
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
