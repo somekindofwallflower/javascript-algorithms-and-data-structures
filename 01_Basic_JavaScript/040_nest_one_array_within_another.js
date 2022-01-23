bcfd42921f93f4adca8a2d8f7ec09aff03402c43
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 19:43:39 +0100

    Create a multidimensional array

---
+++ 01_Basic_JavaScript/040_nest_one_array_within_another.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// When you nest an array inside another it is called  multi-dimensional array.
+
+// Task: Create a nested array called myArray.
+
+const myArray = [["Bulls", 23], ["White Sox", 45]];
