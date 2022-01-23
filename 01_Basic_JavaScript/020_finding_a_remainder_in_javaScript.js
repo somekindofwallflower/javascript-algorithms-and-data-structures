6464a81265291885bf773012fcc5204b9c54ab7a
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 14:40:52 +0100

    Finding the remider of devision between 11 and 3

---
+++ 01_Basic_JavaScript/020_finding_a_remainder_in_javaScript.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// The remainder operator % gives the remainder of the division of two numbers.
+
+/*
+Task: Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+const remainder = 0;
+*/
+
+const remainder = 11 % 3;
+console.log("Remainder", remainder);
