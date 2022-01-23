9930efdb611a5ff6cb0692dc86eec41e97f9a319
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 17:54:46 +0100

    Constructing Strings with variables

---
+++ 01_Basic_JavaScript/030_constructing_strings_with_variables.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// By using the concatenation operator (+), you can insert one or more variables into a string you're building.
+
+/*
+Task: Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well!
+
+const myName = "";
+const myStr = "";
+
+*/
+
+const myName = "Some kind of Wallflower";
+const myStr = "My name is" + myName + "and I am well!";
+
+console.log("My string", myStr);
