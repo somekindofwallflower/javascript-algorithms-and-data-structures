8b6e396d10bf74c3962d01f38d318551fa0c471b
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 20:24:40 +0100

    Created a function that takes as arguments to numbers and prints to the console the sum of these two numbers

---
+++ 01_Basic_JavaScript/050_passing_values_to_functions_with_arguments.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Parameters are variables that act as placeholders for the values that are to be input to a function when it is called
+
+/*
+Task: 1) Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+      2) Call the function with two numbers as arguments.*/
+
+function functionWithArgs(arg1, arg2) {
+    console.log(arg1 + arg2)
+}
+
+functionWithArgs(2, 3);
