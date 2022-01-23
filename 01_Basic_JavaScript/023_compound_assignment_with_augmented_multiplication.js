ca49158571d15bdf0af269555595ba151854c79d
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 15:13:11 +0100

    Compound -* to do the multiplication and assignment in one step

---
+++ 01_Basic_JavaScript/023_compound_assignment_with_augmented_multiplication.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// The *= operator multiplies a variable by a number.
+
+/*
+Task: Convert the assignments for a, b, and c to use the *= operator.
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a = a * 5;
+b = 3 * b;
+c = c * 10;
+
+*/
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
