a8694bf1fcdb55e35ef40183a55d860b5bf489bf
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 21:22:46 +0100

    Create function that returns

---
+++ 01_Basic_JavaScript/051_return_a_value_from_a_function_with_return.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+
+// Task: Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesFive(arg) {
+    return 5 * arg;
+}
+
+timesFive(5);
+console.log("Result: ", timesFive(5));
