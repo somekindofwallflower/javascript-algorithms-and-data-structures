e9881bc2ee351ae0a3d5f33efc012f8368e74c80
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 19:57:16 +0100

    Add element to array using push method

---
+++ 01_Basic_JavaScript/044_manipulate_arrays_with_push.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// An easy way to append data to the end of an array is via the push() function.
+// .push() takes one or more parameters and "pushes" them onto the end of the array.
+
+// Task: Push ["dog", 3] onto the end of the myArray variable.
+
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+myArray.push(["dog", 3]);
+console.log("My array: ", myArray);
