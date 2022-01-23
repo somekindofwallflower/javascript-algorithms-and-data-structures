8248be7afb4f8acd9078da83bcaf0adc50b01ce1
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 15:42:22 +0100

    Quoting string with single quote instead of double quotes

---
+++ 01_Basic_JavaScript/026_quoting_strings_with_single_quotes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// String values in JavaScript may be written with single or double quotes, as long as you start and end with the same type of quote.
+// Unlike some other programming languages, single and double quotes work the same in JavaScript.
+
+/*
+Task: Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+      Right now, the <a> tag in the string uses double quotes everywhere.
+      You will need to change the outer quotes to single quotes so you can remove the escape characters.
+
+      const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";*/
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
