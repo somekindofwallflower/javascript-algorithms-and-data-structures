a012b18e0e5ea032e814cf3e307927215c3bd5af
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 19:47:23 +0100

    Access first element of the array using bracket notation

---
+++ 01_Basic_JavaScript/041_access_array_data_with_indexes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// We can access the data inside arrays using indexes
+// Array indexes are written in the same bracket notation that strings use, except that instead of specifying a character, they are specifying an entry in the array.
+// Like strings, arrays use zero-based indexing, so the first element in an array has an index of 0.
+
+
+// Task: Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+
+const myArray = [50, 60, 70];
+const myData = myArray[0];
+
+console.log("Fist element of: " + myArray + "is: " + myData);
