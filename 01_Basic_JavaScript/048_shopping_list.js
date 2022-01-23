39de632aad1193033da79229ff280b4f1f4835bc
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 20:16:54 +0100

    Create a shooping list with 5 elements

---
+++ 01_Basic_JavaScript/048_shopping_list.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+// The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+// ["Chocolate Bar", 15]
+// There should be at least 5 sub-arrays in the list.
+
+const myList = [];
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Pasta", 3]);
+myList.push(["Coca", 6]);
+myList.push(["Bread", 1]);
+myList.push(["Eggs", 12]);
