64c4f332220246d399c3622b06241804c907d3fc
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 15:10:06 +0100

    Compound -= to do the substraction and assignment in one step

---
+++ 01_Basic_JavaScript/022_compound_assignment_with_augmented_subtraction.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// -= operator subtracts a number from a variable.
+/*
+
+Task: Convert the assignments for a, b, and c to use the -= operator.
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a = a - 6;
+b = b - 15;
+c = c - 1;
+
+*/
+
+let a = 11;
+let b = 9;
+let c = 3;
+a -= 6;
+b -= 15;
+c -= 1;
