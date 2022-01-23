ed49920bbb3ec147fbee27bfe3dece82e63065a1
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 13:54:53 +0100

    Multiply two numbers using * operator

---
+++ 01_Basic_JavaScript/013_multiply_two_numbers_with_javaScript.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// JavaScript uses the * symbol for multiplication of two numbers.
+
+/*
+Task: Change the 0 so that product will equal 80.
+const product = 8 * 0;*/
+
+const product = 8 * 10;
+console.log(product);
