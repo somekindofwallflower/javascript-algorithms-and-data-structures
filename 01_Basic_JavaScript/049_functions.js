9fe95246ef1bfdf140a13bfd5004c0460a144a71
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 20:20:41 +0100

    Create a simple function that prints in the console Hi World

---
+++ 01_Basic_JavaScript/049_functions.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// In JavaScript, we can divide up our code into reusable parts called functions.
+
+/*Task:
+    1) Create a function called reusableFunction which prints the string Hi World to the dev console.
+    2) Call the function.*/
+
+function reusableFunction () {
+    console.log("Hi World");
+}
+
+reusableFunction();
