3f247473c7071da8f44d5aa0e50ce4f4ed8dd0f0
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 13:58:12 +0100

    Divide one number by another using / operator

---
+++ 01_Basic_JavaScript/014_divide_one_number_by_another_with_javascript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// JavaScript uses the / symbol for division.
+
+/*Task: Change the 0 so that the quotient is equal to 2.
+const quotient = 66 / 0;*/
+
+const quotient = 66 / 33;
+console.log(quotient);
