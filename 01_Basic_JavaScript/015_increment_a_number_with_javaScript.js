37f4ac0861983000ca4507042fcc3469edec3eae
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 14:01:17 +0100

    Increment a variable using ++ operator

---
+++ 01_Basic_JavaScript/015_increment_a_number_with_javaScript.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Easily increment or add one to a variable with the ++ operator.
+
+/*
+Task: Change the code to use the ++ operator on myVar.
+
+let myVar = 87;
+// Only change code below this line
+myVar = myVar + 1;
+
+*/
+
+let myVar = 87;
+myVar++;
+console.log(myVar);
