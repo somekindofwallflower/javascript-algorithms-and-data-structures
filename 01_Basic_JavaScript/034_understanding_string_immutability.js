c6ec420aa7630b2b08047eb8257c31782d9676d5
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 18:40:53 +0100

    Change value of a string by reassigning it to the new value

---
+++ 01_Basic_JavaScript/034_understanding_string_immutability.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// In JavaScript, String values are immutable, which means that they cannot be altered once created.
+
+/*
+Task: Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr[0] = "H"; // Change this line
+// Only change code above this line
+
+*/
+
+let myStr = "Jello World";
+myStr= "Hello World";
