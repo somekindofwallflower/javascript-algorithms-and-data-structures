754e4a6f2fb804fa8d3e6239f71cd262ef2828f5
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 14:04:21 +0100

    Decrement a number using -- operator

---
+++ 01_Basic_JavaScript/0016_decrement_a_number_with_javaScript.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Easily decrement or decrease a variable by one with the -- operator.
+
+/*
+Task: Change the code to use the -- operator on myVar.
+
+let myVar = 11;
+// Only change code below this line
+myVar = myVar - 1;
+
+*/
+
+
+let myVar = 11;
+myVar--;
+console.log(myVar);
