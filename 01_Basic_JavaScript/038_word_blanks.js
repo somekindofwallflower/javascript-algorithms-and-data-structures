b7ab157308a98b874689897d22394ffbc40f0b70
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 19:35:12 +0100

    Complete challenge of word blanks

---
+++ 01_Basic_JavaScript/038_word_blanks.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*Task: In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
+You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence.*/
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "The " + myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb;
