de132be5a3da354a15a560aae16e722faf861396
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 19:39:24 +0100

    Store multiple values in array

---
+++ 01_Basic_JavaScript/039_store_multiple_values_in_one_variable_using_javaScript_arrays.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// With JavaScript array variables, we can store several pieces of data in one place.
+
+// Task: Modify the new array myArray so that it contains both a string and a number (in that order).
+
+const myArray = ["test", 13];
+console.log("My array: ", myArray);
