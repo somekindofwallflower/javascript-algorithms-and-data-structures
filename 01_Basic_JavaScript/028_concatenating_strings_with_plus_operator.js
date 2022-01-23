e94f6d87442e015cd345bd8b980f6b134e6cf50e
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 15:55:17 +0100

    Concatinating strings using + operator

---
+++ 01_Basic_JavaScript/028_concatenating_strings_with_plus_operator.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// In JavaScript, when the + operator is used with a String value, it is called the concatenation operator.
+// You can build a new string out of other strings by concatenating them together.
+
+// Task: Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
+
+const myStr = "This is the start." + " " + "This is the end.";
