68cc2e0ef24e241868aaaff01ff4dfb513ed58e8
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 18:34:39 +0100

    Find the first character in a string by using bracket notation

---
+++ 01_Basic_JavaScript/033_use_brack_notation_to_find_the_first_character_in_a_string.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// You can get the value of the first letter of the string by using string[0]
+
+
+/*
+
+Task: Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName; // Change this line
+
+*/
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+console.log("First letter of last name: ", firstLetterOfLastName);
