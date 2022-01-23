7c63a2f8c6773fb8c0d8e2aff70c2a81d2cf82f5
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 14:51:41 +0100

    Compound =+ to do the addition and assignment in one step

---
+++ 01_Basic_JavaScript/021_compound_assignment_with_augmented_addition.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Operators can do both a mathematical operation and assignment in one step.
+// One such operator is the += operator.
+
+/*
+Task: Convert the assignments for a, b, and c to use the += operator.
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+
+*/
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
