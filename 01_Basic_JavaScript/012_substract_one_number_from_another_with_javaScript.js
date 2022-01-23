5245eddd859b980c2473b556b17aabc48fe1e1d2
somekindofwallflower <someone.ana13@gmail.com>
2022-01-23 13:52:06 +0100

    Substracting one number from another using - operator

---
+++ 01_Basic_JavaScript/012_substract_one_number_from_another_with_javaScript.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// JavaScript uses the - symbol for subtraction.
+
+/*
+Task: Change the 0 so the difference is 12.
+const difference = 45 - 0;*/
+
+const difference = 45 - 33;
+console.log(difference);
