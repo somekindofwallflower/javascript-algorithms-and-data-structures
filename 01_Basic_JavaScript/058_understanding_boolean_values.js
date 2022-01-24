0688b70879cca70961c211404f5020f86fa7a9ac
somekindofwallflower <someone.ana13@gmail.com>
2022-01-24 16:52:40 +0100

    Modify method it order to return true

---
+++ 01_Basic_JavaScript/058_understanding_boolean_values.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Booleans may only be one of two values: true or false. They are basically little on-off switches, where true is on and false is off.
+
+// Task: Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+
+
+function welcomeToBooleans() {
+    // return false;
+    return true;
+}
+
+console.log("Welcome to boolean return function value is: ", welcomeToBooleans());
