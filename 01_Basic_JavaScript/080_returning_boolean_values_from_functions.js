b4e9b26de7b4bd509c1cda2e3dfd4b062009699f
somekindofwallflower <someone.ana13@gmail.com>
2022-01-30 23:24:48 +0100

    Return boolean value from function

---
+++ 01_Basic_JavaScript/080_returning_boolean_values_from_functions.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/*
+Task: Fix the function isLess to remove the if/else statements.
+
+    function isLess(a, b) {
+        // Only change code below this line
+        if (a < b) {
+            return true;
+        } else {
+            return false;
+        }
+        // Only change code above this line
+    }
+
+isLess(10, 15);*/
+
+
+function isLess(a, b) {
+    return a < b;
+}
+
+isLess(10, 15);
