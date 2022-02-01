d57e1dc476a4f7a664621767b83589a5a70ee8e7
somekindofwallflower <someone.ana13@gmail.com>
2022-02-01 23:10:17 +0100

    Calculate product in nested loops

---
+++ 01_Basic_JavaScript/101_nesting_loops.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Task: Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+function multiplyAll(arr) {
+    let product = 1;
+        for(let i = 0; i < arr.length; i++) {
+            for(let j = 0; j < arr[i].length; j++) {
+                product *= arr[i][j];
+            }
+        }
+    return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
