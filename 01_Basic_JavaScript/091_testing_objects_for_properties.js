f03be957427236e83bb292249dccfea755f9dbe4
somekindofwallflower <someone.ana13@gmail.com>
2022-02-01 21:40:01 +0100

    Create a function that taskes as an orgument an object and a string and do the check if object has that property or not

---
+++ 01_Basic_JavaScript/091_testing_objects_for_properties.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Sometimes it is useful to check if the property of a given object exists or not. We can use the .hasOwnProperty(propname) method of objects to determine if that object has the given property name. .hasOwnProperty() returns true or false if the property is found or not.
+
+// Task: Modify the function checkObj to test if an object passed to the function (obj) contains a specific property (checkProp). If the property is found, return that property's value. If not, return "Not Found".
+
+function checkObj(obj, checkProp) {
+    if(obj.hasOwnProperty(checkProp)) {
+        return obj[checkProp];
+    } else {
+        return "Not Found";
+    }
+}
