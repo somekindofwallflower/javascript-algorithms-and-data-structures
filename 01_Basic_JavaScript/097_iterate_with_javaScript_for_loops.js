823b36ccce87c7a2b5d972083f530c74d63ca901
somekindofwallflower <someone.ana13@gmail.com>
2022-02-01 22:43:41 +0100

    Iterate with javaScript for loops

---
+++ 01_Basic_JavaScript/097_iterate_with_javaScript_for_loops.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Task: Use a for loop to push the values 1 through 5 onto myArray.
+
+const myArray = [];
+
+for(let i = 1; i <= 5; i++) {
+    myArray.push(i);
+}
+
+console.log(myArray);
