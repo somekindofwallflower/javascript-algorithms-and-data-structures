6b6bf86e6fccbdddfda9234516e63b3a9096bdd3
somekindofwallflower <someone.ana13@gmail.com>
2022-02-01 22:57:10 +0100

    Count backwords with a for loop

---
+++ 01_Basic_JavaScript/099_count_backwards_with_a_for_loop.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Task: Push the odd numbers from 9 through 1 to myArray using a for loop.
+
+const myArray = [];
+
+for(let i = 9; i >= 1; i--) {
+    myArray.push(i);
+}
+
+console.log(myArray);
