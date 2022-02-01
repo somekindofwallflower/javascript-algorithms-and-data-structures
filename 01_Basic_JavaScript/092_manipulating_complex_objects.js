422aa6633b1dd520a2f2c84ed94d9009917f3931
somekindofwallflower <someone.ana13@gmail.com>
2022-02-01 21:45:29 +0100

    Addind a new object to myMusic array

---
+++ 01_Basic_JavaScript/092_manipulating_complex_objects.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Task: Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    }
+];
+myMusic.push({
+    "artist": "Anson",
+    "title": "Emerald Eyes",
+    "release_year": 2019,
+    "formats": [
+        "CD",
+        "LP"
+    ],
+})
