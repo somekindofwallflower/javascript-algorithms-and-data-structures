c6d91333620ea75ad09e7be7be73b8a411ce088e
somekindofwallflower <someone.ana13@gmail.com>
2022-02-01 22:39:45 +0100

    While loop

---
+++ 01_Basic_JavaScript/096_iterate_with_javaScript_while_loops.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Task: Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+
+const myArray = [];
+let i = 5;
+
+while (i <= 5 && i >= 0) {
+    myArray.push(i);
+    i--;
+}
