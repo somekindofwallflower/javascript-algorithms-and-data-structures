f578d2273390981583ac95d840fcc3c9666565d2
somekindofwallflower <someone.ana13@gmail.com>
2022-02-01 20:55:36 +0100

    Create myDog object

---
+++ 01_Basic_JavaScript/083_build_javaScript_objects.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+/*
+Task: Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+    You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.*/
+
+const myDog = {
+   name: "Max",
+    legs: 4,
+    tails: 1,
+    friends: ["Joe", "Ana", "Bob"]
+};
