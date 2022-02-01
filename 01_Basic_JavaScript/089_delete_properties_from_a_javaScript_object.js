9bca1a153d12e917bca5024de866cd018ccb3b14
somekindofwallflower <someone.ana13@gmail.com>
2022-02-01 21:21:27 +0100

    Delete a object proeprty

---
+++ 01_Basic_JavaScript/089_delete_properties_from_a_javaScript_object.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Task: Delete the tails property from myDog. You may use either dot or bracket notation.
+
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+delete myDog.tails;
