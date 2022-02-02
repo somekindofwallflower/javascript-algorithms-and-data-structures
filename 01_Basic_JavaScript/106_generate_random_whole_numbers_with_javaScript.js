b3f4da2c90b8e2115df9386169f7c50721dbc6b1
somekindofwallflower <someone.ana13@gmail.com>
2022-02-02 22:42:00 +0100

    Generate whole numbers between 1 and 9

---
+++ 01_Basic_JavaScript/106_generate_random_whole_numbers_with_javaScript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Task: Generate and return a random whole number between 0 and 9.
+
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
+
+console.log(randomWholeNum());
