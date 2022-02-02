e27208a62ba0fea212694837e7c26f07f78315c6
somekindofwallflower <someone.ana13@gmail.com>
2022-02-02 22:55:09 +0100

    Convert to integer - parseInt function

---
+++ 01_Basic_JavaScript/108_use_the_parse_int_function.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// The parseInt() function parses a string and returns an integer.
+
+// Task: Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
+
+function convertToInteger(str) {
+ return parseInt(str);
+}
+
+convertToInteger("56");
